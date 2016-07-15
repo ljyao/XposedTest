59e2843f2e6fdeb9e3333a0f7130307a831677cb
李进耀 <lijinyao@oneniceapp.com>
2016-07-15 17:31:01 +0800

    参数修改

--- app/src/main/java/com/example/shine/xposedtest/Nice.java
+++ app/src/main/java/com/example/shine/xposedtest/Nice.java
@@ -1,10 +1,11 @@
 package com.example.shine.xposedtest;
 
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 /**
  * Created by shine on 16/7/15.
@@ -15,6 +16,10 @@ public class Nice implements IXposedHookLoadPackage {
         findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Object user = param.args[0];
+                XposedHelpers.setIntField(user, "showsNum", 888888);
+                XposedHelpers.setIntField(user, "zansNum", 888888);
+                XposedHelpers.setIntField(user, "showsNum", 888888);
                 // this will be called before the clock was updated by the original method
             }
 
